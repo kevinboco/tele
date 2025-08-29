d2a255fa5399556639f6bc3eb2c78e1318e89e0a
kevinboco <kevinalexanderboca@gmail.com>
2025-08-29 11:40:34 -0500

    ya se arreglo funciona el bot

--- eliminar.php
+++ eliminar.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 
 $sql = "DELETE FROM viajes WHERE id=$id";
 if($conexion->query($sql)){
-    echo "<script>alert('Viaje eliminado');window.location='index.php';</script>";
+    echo "<script>alert('Viaje eliminado');window.location='index2.php';</script>";
 }else{
     echo "Error: ".$conexion->error;
 }
