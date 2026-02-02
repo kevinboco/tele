5b125b91309a90d60697c8673dd0ca097d9b11ee
kevinboco <kevinalexanderboca@gmail.com>
2026-02-02 14:29:33 -0500

    ver

--- veri.php
+++ veri.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
 include("nav.php");
+session_start();
+
 $conn = new mysqli("mysql.hostinger.com", "u648222299_keboco5", "Bucaramanga3011", "u648222299_viajes");
 if ($conn->connect_error) { die("Error conexión BD: " . $conn->connect_error); }
 $conn->set_charset('utf8mb4');
