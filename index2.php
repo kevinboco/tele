074e451f03de8dc22bdeb101962ebd94ef9d3c6a
kevinboco <kevinalexanderboca@gmail.com>
2026-05-07 08:21:58 -0500

    index2

--- index2.php
+++ index2.php
@@ -918,24 +918,7 @@ if (isset($_SESSION['error'])) unset($_SESSION['error']);
 <?php include("nav.php"); ?>
 
 <!-- NAVEGACIÓN SUPERIOR -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-    <div class="container">
-        <a class="navbar-brand" href="?">🚗 Sistema de Viajes</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link <?= $accion == 'listar' ? 'active' : '' ?>" href="?">📋 Listado</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $accion == 'crear' ? 'active' : '' ?>" href="?accion=crear">➕ Nuevo Viaje</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+
 
 <div class="container py-4">
     <!-- MENSAJES -->
