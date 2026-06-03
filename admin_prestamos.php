b1c0b955acdee644b2944a21a74b32114b0ad208
kevinboco <kevinalexanderboca@gmail.com>
2026-06-03 13:34:43 -0500

    admin prestamo funcional

--- admin_prestamos.php
+++ admin_prestamos.php
@@ -1251,7 +1251,7 @@ $(document).ready(function() {
   }
 })();
 
-/* Eliminar: crea un form temporal POST para no anidar forularios */
+/* Eliminar: crea un form temporal POST para no anidar forularios *
 function submitDelete(id){
   if(!confirm('¿Eliminar #'+id+'?')) return;
   const f = document.createElement('form');
